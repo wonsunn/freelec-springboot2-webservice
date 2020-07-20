b10e7e9faa491b7bdd603413b53fc822fbf9da1e
Wonssun <heidencena1@gmail.com>
2020-07-20 15:36:22 +0900

    ch3.4(등록/수정/조회 api 생성)

---
+++ src/main/java/com/wons/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.wons.book.springboot.service.posts;
+
+import com.wons.book.springboot.domain.posts.Posts;
+import com.wons.book.springboot.domain.posts.PostsRepository;
+import com.wons.book.springboot.web.dto.PostsResponseDto;
+import com.wons.book.springboot.web.dto.PostsSaveRequestDto;
+import com.wons.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+}
