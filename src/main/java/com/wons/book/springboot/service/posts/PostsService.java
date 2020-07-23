8f599b7f74864d33a08f9c36ea8f9c030e331a46
Wonssun <heidencena1@gmail.com>
2020-07-23 11:11:02 +0900

    ch4(4.4~4.5) 게시글 전체조회/수정/삭제 추가

--- src/main/java/com/wons/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/wons/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.wons.book.springboot.service.posts;
 
 import com.wons.book.springboot.domain.posts.Posts;
 import com.wons.book.springboot.domain.posts.PostsRepository;
+import com.wons.book.springboot.web.dto.PostsListResponseDto;
 import com.wons.book.springboot.web.dto.PostsResponseDto;
 import com.wons.book.springboot.web.dto.PostsSaveRequestDto;
 import com.wons.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,7 +26,7 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
@@ -33,6 +37,17 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 
 }
